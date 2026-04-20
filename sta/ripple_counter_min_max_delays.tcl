1d2ef2662189b0368a6c82f4cab1312b9afcc24c
Madan K <madank4421@gmail.com>
2026-04-20 16:50:19 +0530

    Create ripple_counter_min_max_delays.tcl

---
+++ sta/ripple_counter_min_max_delays.tcl
@@ -0,0 +1,15 @@
+# Load Liberty Libraries (standard cell + IPs)
+read_liberty -min ~/Desktop/work/tools/openlane_working_dir/pdks/skywater-pdk/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib
+read_liberty -max ~/Desktop/work/tools/openlane_working_dir/pdks/skywater-pdk/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib
+
+# Read Synthesized Netlist
+read_verilog ~/madan/synthesis/ripple_counter.synth.v
+
+# Link the Top-Level Design
+link_design ripple_counter
+
+# Apply SDC Constraints
+read_sdc ~/madan/sta/ripple_counter_synthesis.sdc
+
+# Generate Timing Report
+report_checks
